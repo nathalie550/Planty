e2986f06aec202ce660049527b91e832d0601ce8
Nathalie <jeannathalie550@gmail.com>
2023-09-07 15:16:25 +0200

    Ajout du template-formulaire.php rajout bouton commander

--- template-formulaire.php
+++ template-formulaire.php
@@ -8,7 +8,7 @@
 
 <section class="code">
     <div class="informations">
-        <h3>Vos informations</h3>
+        <h4>Vos informations</h4>
 
         <?php echo do_shortcode('[contact-form-7 id="322" title="Vos informations"]'); ?>
     </div>
@@ -16,7 +16,11 @@
     <div class="divider2"></div>
 
     <div class="livraison">
-        <h3>Livraison</h3>
+        <h4>Livraison</h4>
         <?php echo do_shortcode('[contact-form-7 id="324" title="Livraison"]'); ?>
     </div>
+
 </section>
+<div class="bouton-commander">
+    <p><input type="submit" value="Commander"></p>
+</div>
